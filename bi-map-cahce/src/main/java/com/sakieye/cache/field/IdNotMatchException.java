8b733cb22d05f550f5801ead3a733d495c019aad
Y.H. Kuo <yhkuo41@gmail.com>
2022-03-03 22:44:35 +0800

    change exception

--- bi-map-cahce/src/main/java/com/sakieye/cache/field/IdNotMatchException.java
+++ bi-map-cahce/src/main/java/com/sakieye/cache/field/IdNotMatchException.java
@@ -1,6 +1,6 @@
 package com.sakieye.cache.field;
 
-public class IdNotMatchException extends Exception {
+public class IdNotMatchException extends IllegalStateException {
   public IdNotMatchException(String msg) {
     super(msg);
   }
